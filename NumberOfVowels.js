ccea174be303605c9304645d65e8f7f78c4612fe
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-01-19 02:16:01 -0500

    Finished kata 3

---
+++ NumberOfVowels.js
@@ -0,0 +1,17 @@
+let numberOfVowels = function (data) {
+  let counter = 0;
+  let vowels = ["a", "e", "i", "o", "u"]; // Create vowels array to match for vowels later
+  let dataArray = data.split(''); // Split string into array of letters
+
+  // Go through each letter and see if the vowels array has it
+  dataArray.forEach(letter => {
+    if (vowels.includes(letter)) {
+      counter++;
+    }
+  });
+  return counter;
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));

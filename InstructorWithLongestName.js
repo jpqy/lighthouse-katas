97dd4ee7f0882e6b1570ab8e8519c64d46a5dd64
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-01-19 13:38:29 -0500

    Finished kata 4

---
+++ InstructorWithLongestName.js
@@ -0,0 +1,23 @@
+const instructorWithLongestName = function (instructors) {
+  indexOfLongest = 0; // Keep track of index of instructor with longest name
+
+  // Go through instructors and assign indexOfLongest to current index if current index's name is longer
+  instructors.forEach((instructor, index) => {
+    if (instructor.name.length > instructors[indexOfLongest]["name"].length) {
+      indexOfLongest = index;
+    }
+  });
+  return instructors[indexOfLongest];
+};
+
+console.log(instructorWithLongestName([
+  { name: "Samuel", course: "iOS" },
+  { name: "Jeremiah", course: "Web" },
+  { name: "Ophilia", course: "Web" },
+  { name: "Donald", course: "Web" }
+]));
+console.log(instructorWithLongestName([
+  { name: "Matthew", course: "Web" },
+  { name: "David", course: "iOS" },
+  { name: "Domascus", course: "Web" }
+]));
